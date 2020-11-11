3f926ee9e8dc46556a6e6cf04ca1f724642431b9
QyuBot <tkrlso123@gmail.com>
2020-11-12 01:21:09 +0900

    Remove unused codes, add kakao sdk in build.gradle

--- app/src/main/java/com/example/capston_park_application/MainActivity.java
+++ app/src/main/java/com/example/capston_park_application/MainActivity.java
@@ -1,13 +1,13 @@
-package com.example.capston_park_application; // 본인 프로젝트이름에 맞게 변경
-
+package com.example.capston_park_application;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) { //savedInstanceState = 세로, 가로 화면변경시 전역변수 초기화를 방지
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
