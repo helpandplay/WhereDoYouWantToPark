cc5255bdfd6b21e953dbc7db075b40d9f8b61d6f
helpandplay <helpandplay.kr@gmail.com>
2020-11-12 00:56:48 +0900

    레이아웃 복사해왔음

--- app/src/main/java/com/example/capston_park_application/MainActivity.java
+++ app/src/main/java/com/example/capston_park_application/MainActivity.java
@@ -1,45 +1,16 @@
-package com.example.capston_park_application;
-import androidx.annotation.NonNull;
+package com.example.capston_park_application; // 본인 프로젝트이름에 맞게 변경
+
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) { //savedInstanceState = 세로, 가로 화면변경시 전역변수 초기화를 방지
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tvtest = (TextView) findViewById(R.id.text_view_id);
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference myRef = mDatabase.child("users/");
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user = snapshot.child("useridtest").getValue(User.class);
-                tvtest.setText(user.email);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+        // 다른 클래스 함수 호출을 여기에 적으면 버튼들 정상 작동
     }
 }
